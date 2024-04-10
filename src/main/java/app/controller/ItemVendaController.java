cba42b8caccd10c2b79453fd92260c18f19bbd54
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-04-09 21:39:44 -0400

    ADD itenvenda CRUD metods controller and service

--- src/main/java/app/controller/ItemVendaController.java
+++ src/main/java/app/controller/ItemVendaController.java
@@ -1,10 +1,96 @@
 package app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import app.entity.ItemVenda;
+import app.service.ItemVendaService;
 
 @RequestMapping("/api/itemvenda")
 @RestController
 public class ItemVendaController {
+	
+	@Autowired
+	private ItemVendaService itemVendaService;
 
+	@PostMapping("/save")
+	public ResponseEntity<String> save(@RequestBody ItemVenda itemVenda){
+
+		try {
+			
+			String mensagem = this.itemVendaService.save(itemVenda);
+			return new ResponseEntity<>(mensagem,HttpStatus.CREATED);	
+			
+		} catch (Exception e) {
+
+			return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);		
+
+		}
+
+	}
+
+	@GetMapping("/listAll")
+	public ResponseEntity <List<ItemVenda>> listAll(){
+
+		try {
+			List<ItemVenda> lista = this.itemVendaService.listAll();
+			return new ResponseEntity<>(lista, HttpStatus.CREATED);
+		} catch (Exception e) {			
+			return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);		
+
+		}
+
+	}
+
+	@GetMapping("/findById/{idCliente}")
+	public ResponseEntity <ItemVenda> findById(@PathVariable long idItemVenda){
+
+		try {
+			ItemVenda itemVenda = this.itemVendaService.findById(idItemVenda);
+			return new ResponseEntity<>(itemVenda, HttpStatus.CREATED);
+		} catch (Exception e) {			
+			return new ResponseEntity<>(null,HttpStatus.BAD_GATEWAY);		
+
+		}
+
+	}
+	
+	@PutMapping("/update/{idCliente}")
+	public ResponseEntity<String> update(@RequestBody ItemVenda itemVenda, @PathVariable long idItemVenda){
+
+		try {
+			String mensagem = this.itemVendaService.update(itemVenda, idItemVenda);
+			return new ResponseEntity<>(mensagem,HttpStatus.OK);		
+		} catch (Exception e) {
+
+			return new ResponseEntity<>(null,HttpStatus.BAD_GATEWAY);		
+
+		}
+
+	}
+	
+	@DeleteMapping("/delete/{idCliente}")
+	public ResponseEntity<String> delete(@PathVariable long idItemVenda){
+
+		try {
+			String mensagem = this.itemVendaService.delete(idItemVenda);
+			return new ResponseEntity<>(mensagem,HttpStatus.OK);		
+		} catch (Exception e) {
+
+			return new ResponseEntity<>(null,HttpStatus.BAD_GATEWAY);		
+
+		}
+
+	}
 }
--- src/main/java/app/service/ItemVendaService.java
+++ src/main/java/app/service/ItemVendaService.java
@@ -1,8 +1,46 @@
 package app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import app.entity.ItemVenda;
+import app.repository.ItemVendaRepository;
 
 @Service
 public class ItemVendaService {
+	
+	@Autowired
+	private ItemVendaRepository itemVendaRepository;
+	
+	public String save (ItemVenda itemVenda) {
+		this.itemVendaRepository.save(itemVenda);
+		return itemVenda.getValorUnitario() + " Foi adicionado";
+	}
+	
+	public List <ItemVenda> listAll () {
+	return this.itemVendaRepository.findAll();
+		
+	}
+	
 
+	public ItemVenda findById(long idItemVenda) {
+
+		ItemVenda itemVenda = this.itemVendaRepository.findById(idItemVenda).get();
+		return itemVenda;
+
+	}
+	
+	public String update (ItemVenda itemVenda, long idItemVenda) {
+		itemVenda.setIdItemVenda(idItemVenda);
+		this.itemVendaRepository.save(itemVenda);
+		return "O " + itemVenda.getValorUnitario() + " Foi atualizado";
+		
+	}
+	
+	public String delete (long idItemVenda) {
+		this.itemVendaRepository.deleteById(idItemVenda);
+		return " Venda deletada";
+	}
 }

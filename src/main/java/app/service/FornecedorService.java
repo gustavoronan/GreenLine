cef02e94f23edc75876da5a5cd3078e9fe4b0fe3
Gabriel Pletsch <gabrielexxonpletsch@gmail.com>
2024-04-09 21:52:19 -0300

    ADD Repository and Service

--- src/main/java/app/service/FornecedorService.java
+++ src/main/java/app/service/FornecedorService.java
@@ -1,8 +1,60 @@
 package app.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import app.entity.Fornecedor;
+import app.repository.FornecedorRepository;
 
 @Service
 public class FornecedorService {
 
-}
+        @Autowired
+        private FornecedorRepository fornecedorRepository; 
+
+        //CRUD
+
+
+        //Create
+        public String save(Fornecedor fornecedor) {
+            this.fornecedorRepository.save(fornecedor);
+            return fornecedor.getNomeFornecedor()+" Fornecedor salvo com sucesso!!";
+        }
+
+
+        //Read
+        public List<Fornecedor> listAll(){ 
+            return this.fornecedorRepository.findAll(); 
+        }
+        public Fornecedor findById(long idFornecedor) { 
+            Fornecedor fornecedor = this.fornecedorRepository.findById(idFornecedor).get(); 
+            return fornecedor; 
+        }
+
+
+        //Update
+        public String update (long id, Fornecedor fornecedor) { 
+            fornecedor.setIdFornecedor(id); 
+            this.fornecedorRepository.save(fornecedor);
+            return fornecedor.getNomeFornecedor()+" Atualizado fornecedor com sucesso!!";
+        }
+
+
+        //Delete
+        public String delete (long idFornecedor) {
+            this.fornecedorRepository.deleteById(idFornecedor);;
+            return " Fornecedor deletado com sucesso";
+        }
+
+
+        //-- Consulta ao SGDB (Read)--
+
+
+        public List<Fornecedor> findByNomeFornecedor(String nomeFornecedor){
+            return this.fornecedorRepository.findByNomeForne(nomeFornecedor);
+        }
+
+
+    }

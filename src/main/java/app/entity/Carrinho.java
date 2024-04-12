12fa4eea0d76c3726d9d0c78b627083c34bed201
lucas schneider <lucaseduschneiderengs@gmail.com>
2024-04-11 21:12:20 -0400

    UPDATE entity add relacionamentos

--- src/main/java/app/entity/Carrinho.java
+++ src/main/java/app/entity/Carrinho.java
@@ -1,9 +1,19 @@
 package app.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +28,17 @@ public class Carrinho {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idCarrinho;
+	@NotBlank (message = " Descricao nao pode estar vazio")
 	private String descricaoCarrinho;
-	private String valorCarrinho;
+	@NotNull (message = " Valor nao pode estar nulo")
+	private double valorCarrinho;
+	
+	@OneToMany (mappedBy = "carrinho")
+	@JsonIgnoreProperties ("carrinho")
+	private List<ItemCarrinho> itemCarrinho;
+	
+	@ManyToOne (cascade = CascadeType.ALL)
+	@JsonIgnoreProperties ("carrinho")
+	private Usuario usuario;
+	
 }

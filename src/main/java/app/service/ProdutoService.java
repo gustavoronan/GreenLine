037cb35bba6cf653060e7f63ea09ce3cce469b4b
gustavo <gustavoronn@gmail.com>
2024-08-23 21:00:28 -0300

    feat: ProdutoService logAud

--- src/main/java/app/service/ProdutoService.java
+++ src/main/java/app/service/ProdutoService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import app.entity.Log;
 import app.entity.Produto;
+import app.repository.LogRepository;
 import app.repository.ProdutoRepository;
 
 @Service
@@ -13,11 +16,20 @@ public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private LogService logService;
+
 	
-	public String save(Produto produto) {
-		produtoRepository.save(produto);
-		return produto.getNomeProduto() + " salvo com sucesso!";
-	}
+	 public String save(Produto produto) {
+
+	        // Gerar log antes de salvar o produto
+	        logService.gerarLog("SAVE", "Produto", produto.getIdProduto(), null);
+
+	        // Salvar o produto no banco de dados
+	        produtoRepository.save(produto);
+
+	        return produto.getNomeProduto() + " salvo com sucesso!";
+	    }
 	
 	public List<Produto> listAll() {
 		return produtoRepository.findAll();
@@ -27,13 +39,33 @@ public class ProdutoService {
 		return produtoRepository.findById(idProduto).get();
 	}
 	
-	public String update(long idProduto, Produto produto) {
-		produto.setIdProduto(idProduto);
-		produtoRepository.save(produto);
-		return "O produto " + produto.getNomeProduto() + " foi atualizado com sucesso!";
-	}
+	public String update(long idProduto, Produto produtoAtualizado) {
+		
+		// Buscar o produto atual do banco de dados
+        Produto produtoAntigo = produtoRepository.findById(idProduto).get();
+        String nomeAntigo = produtoAntigo.getNomeProduto();
+        String nomeNovo = produtoAtualizado.getNomeProduto();
+        double valorAntigo = produtoAntigo.getValorProduto();
+        double valorNovo = produtoAtualizado.getValorProduto();
+        String formato = "Atualizado: Nome antigo = %s, Nome novo = %s; Preço antigo = %.2f, Preço novo = %.2f";
+        String detalhes = String.format(formato, nomeAntigo, nomeNovo, valorAntigo, valorNovo);
+        
+        System.out.println("Produto antigo: Nome = " + produtoAntigo.getNomeProduto() + ", Preço = " + produtoAntigo.getValorProduto());
+        // Atualizar o ID do produto no objeto produtoAtualizado
+        produtoAtualizado.setIdProduto(idProduto);
+        System.out.println("Produto atualizado: Nome = " + produtoAtualizado.getNomeProduto() + ", Preço = " + produtoAntigo.getValorProduto());
+
+        // Salvar o produto atualizado no repositório
+        produtoRepository.save(produtoAtualizado);
+        System.out.println("Detalhes do log: " + detalhes);
+        logService.gerarLog("UPDATE", "Produto", idProduto, detalhes);
+
+        return "O produto " + produtoAtualizado.getNomeProduto() + " foi atualizado com sucesso!";
+    }
+	
 	
 	public String delete(long idProduto) {
+		logService.gerarLog("DELETE", "Produto", idProduto, null);
 		produtoRepository.deleteById(idProduto);
 		return "Produto deletado com sucesso!";
 	}

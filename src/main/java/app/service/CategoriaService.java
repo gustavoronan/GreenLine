5f0a8a70cf1c7c15dafe9a93fce6d09a937c40de
gustavo <gustavoronn@gmail.com>
2024-08-24 21:43:37 -0300

    add: log add at categoria

--- src/main/java/app/service/CategoriaService.java
+++ src/main/java/app/service/CategoriaService.java
@@ -12,15 +12,22 @@ public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LogService logService;
 	
 	public String save(Categoria categoria) {
 		this.categoriaRepository.save(categoria);
+		// Salvar o produto no banco de dados
+        categoriaRepository.save(categoria);
+        // Gerar log antes de salvar o produto
+        logService.gerarLog("SAVE", "Produto", categoria.getIdCategoria(), null);
 		return categoria.getDescricao()+" descrição adicionada";
 	}
 	
 	public List<Categoria> listAll() {
 		return this.categoriaRepository.findAll();
 	}
+	
 	public Categoria findById(long idCategoria) {
 		Categoria categoria = this.categoriaRepository.findById(idCategoria).get();
 		return categoria;
